75264e0ecc562234e9ffed5868645e0c0fc97bf0
Booysen.n <Booysen.n@LAB-2-PC6>
2024-08-20 08:57:32 +0200

    Added Models and Migrations

---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookManager.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+        public ICollection<BookAuthor> BookAuthors { get; set; }
+    }
+}
